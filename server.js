1381c502506f6ce07a006a036c562130d7f47429
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-10-17 22:46:30 +0000

    path for login page made

--- server.js
+++ server.js
@@ -37,11 +37,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const loginRoutes = require("./routes/login");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/login", loginRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -49,6 +51,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Separate them into separate routes files (see above).
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
   res.render("index");
 });
 
