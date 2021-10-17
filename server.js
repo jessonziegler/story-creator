69478d0f13a5ec24df67db051c6a987a861c58ce
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-10-17 18:44:50 -0400

    add registers route

--- server.js
+++ server.js
@@ -37,20 +37,28 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const registersRoutes = require("./routes/register");
+const { supportsColor } = require("supports-color");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/registers", registersRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/registers", (req, res) => {
+  res.render("index");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
