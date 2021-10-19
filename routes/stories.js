df2636b240b4e474a82040b44f9f979b369f86c7
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-10-19 14:20:39 -0400

    update post story front-end

--- routes/stories.js
+++ routes/stories.js
@@ -3,6 +3,18 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+  // router.use((req, res, next) => {
+  //   const user_id = req.cookies.user_id;
+  //   //look them up in the database and send error message.
+  //   if(!user_id){
+  //     return res.send("Error: User not found. Please login")
+  //   }
+  //   next();
+  // })
+
+
+  //get all stories from the stories database
   router.get("/", (req, res) => {
     let query = `SELECT * FROM stories`;
     db.query(query)
@@ -18,16 +30,43 @@ module.exports = (db) => {
   });
 
 
-  // POST /products
+  // //GET /stories/:id
+  // router.get("/:id", (req, res) => {
+  //   id = req.params.id
+  //   let query = `SELECT * FROM stories JOIN users ON users_id = users.id WHERE id = $1`, [id]; //FIX THIS QUERY
+  //   db.query(query)
+  //     .then(data => {
+  //       const stories = data.rows[0];
+  //       res.json({ stories });
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // });
+
+
+  // POST /stories to the stories database
   router.post('/', (req, res) => {
-    const {title, content} = req.body;
-    db.createStoryElement(title, content)
-      .then(() => {
-        // res.redirect('/products');
-        // res.status(201).send();
-        res.json({ success: true });
+    console.log("console log from stories.js " + JSON.stringify(req.body));
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(title);
+    console.log(content);
+    let query = `INSERT INTO stories (title, content, user_id) VALUES ('${title}', '${content}',1)`; //What is user id
+    db.query(query)
+      .then(data => {
+        const stories = data.rows;
+        res.json({ stories });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
       });
   });
+
 
 
 
