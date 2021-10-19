211c77c5b7564463ad9ef7d12b26fae9663046fd
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-10-19 00:36:30 -0400

    insert values to users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Anselm', 'anselm@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Allison', 'allison@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Suzy', 'suzy@gmail.com', 'password');
+
