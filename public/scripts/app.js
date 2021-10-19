37f213805fce17773466896e93c63a05197d9ae0
Mohammed Ali Al-Hassan <ali.alhassan30@gmail.com>
2021-10-19 20:28:14 +0000

    contributions added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,17 @@
 // Client facing scripts here
-// A $( document ).ready() block.
 
 
 const loadStories = function () {
-  $.ajax({
-    method: 'GET',
-    url: "/api/stories",
-    dataType: 'JSON'
-  })
-  .done(function(data) {
-    console.log("Sucess: loading stories call render");
-    console.log(data.stories)
-    renderStories(data.stories);
-  })
-  .fail(err => console.log(`Error: #{err.message}`))
+    $.get('/api/stories')
+      .then((data) => {
+        console.log(data.stories);
+        renderStories(data.stories);
+      });
   }
-
   loadStories();
-
-
 
 const renderStories = function(stories) {
 // loops through tweets
-
   $('#posted-stories-container').empty();
   for(const story of stories){
     // calls createStoryElement for each story
@@ -45,30 +34,25 @@ function createStoryElement(storyData) {
 
   const markup = `
    <article class="story-data">
-
      <div class="story-header">
        <div>
-
          <h3 class="story-title">${storyData.title}</h3>
-
-
      <p class="story-content">${escape(storyData.content)}</p>
-
      <button type="button" class="btn btn-primary">Edit</button>
-     <button type="button" class="btn btn-info">Contributions</button>
+     <button type="button" id="contribution" class="btn btn-info">Contributions</button>
+
+     <form class="contribute-form">
+       <textarea name="submit" placeholder= "Share your imagination with us!"></textarea><br/>
+       <input id="submit" class="btn btn-primary" type="submit" value="Submit">
+       </form>
      <button type="button" class="btn btn-danger">Delete</button>
-
      <hr class="solid">
-
      <footer class="story-footer">
        <div class ="story-icons">
-         <i class="fas fa-flag"></i>
-         <i class="fas fa-retweet"></i>
          <i class="fas fa-heart"></i>
          <hr class="solid">
        </div> <br/>
      </footer>
-
    </article>
    `;
  return markup;
@@ -77,45 +61,45 @@ function createStoryElement(storyData) {
 
   $( document ).ready(function() {
     console.log("Dom is ready")
-    loadStories();
-
-
    // to check if textarea is empty and no white space
 
-
-   $('.content').submit( function (event) {
+   const $newStories = $('#new-product-form');
+   $newStories.submit( function (event) {
     console.log("Submit Story Button clicked and handler for submit story button is called");
     event.preventDefault(); //cancel the submit action by calling .preventDefault()
+    const data = $newStories.serialize();
+    console.log(data);
 
     if (!$.trim($(".content").val())) {
       return $('#error').text('❗️Error: Please enter text');
     }
-
      $('#error').text('');
-
-
     /** jQuery .serialize() function turns a set of form data into a query string.
      * This serialized data should be sent to the server
      * in the data field of the AJAX POST request
      */
-    let storyText = $(this).serialize();
-
-    console.log(storyText);
+   /* let storyText = $(this).serialize(); */
 
     $.ajax({
       type: "POST",
-      url: '/api/stories',
-      data: storyText
+      url: "/api/stories",
+      data
     })
     .then(() => {
+      console.log('so far, so good')
       $('.content').val('');
-
-      loadTweets();
+      loadStories();
     })
-
   });
 
+  const $contribution = $('#contribution');
+  const $contributeForm = $('.contribute-form');
 
-
+  $('body').on('submit', '.contribute-form', function (event) {
+    console.log("User wants to add to a story!")
+    event.preventDefault();
+    const data = $(this).serialize();
+    console.log($(this));
+    $(this).parent().find('.story-content').append(data);
+  })
 });
-
